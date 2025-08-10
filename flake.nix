16905a0cd4cf35a3429f396069d0ece34b01bca5
Pawel Lisewski <dev@palekiwi.com>
2025-08-10 11:06:59 +0800

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Prompt Assist";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, fenix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      rustToolchain = fenix.packages.${system}.stable.toolchain;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell
+        {
+          buildInputs = [
+            rustToolchain
+            pkgs.rust-analyzer
+            pkgs.cargo-expand
+          ];
+
+          shellHook = ''
+            echo "Rust development environment ready!"
+            echo "Rust version: $(rustc --version)"
+          '';
+        };
+    };
+}
